cf1df4aeb72026c1b0cf82901553dbcc07f94f02
student2ua <student2ua@gmail.com>
2013-08-26 21:43:51 +0300

    tree sample refactoring + add popup menu

--- src/com/tor/swing/tree/TreeEditTest.java
+++ src/com/tor/swing/tree/TreeEditTest.java
@@ -1,34 +1,24 @@
 package com.tor.swing.tree;
 
-import org.apache.log4j.Logger;
-
 import javax.swing.*;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeNode;
-import javax.swing.tree.TreePath;
+import javax.swing.tree.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 /**
- * Created by IntelliJ IDEA.
  * User: tor
  * Date: 23.12.2008
  * Time: 15:53:36
- * To change this template use File | Settings | File Templates.
  */
 public class TreeEditTest {
-    private static final Logger log = Logger.getLogger(TreeEditTest.class);
+
     public static void main(String[] args) {
         JFrame frame = new TreeEditFrame();
-        frame.show();
+        frame.setVisible(true);
     }
 }
 
-class TreeEditFrame extends JFrame implements ActionListener {
+class TreeEditFrame extends JFrame {
 
     private DefaultTreeModel model;
     private JTree tree;
@@ -40,11 +30,7 @@ class TreeEditFrame extends JFrame implements ActionListener {
     public TreeEditFrame() {
         setTitle("TreeEditTest");
         setSize(300, 200);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // construct tree
 
@@ -53,26 +39,33 @@ class TreeEditFrame extends JFrame implements ActionListener {
         tree = new JTree(model);
         tree.setEditable(true);
 
+
         // add scroll pane with tree to content pane
 
         Container contentPane = getContentPane();
-        JScrollPane scrollPane = new JScrollPane(tree);
-        contentPane.add(scrollPane, "Center");
+        contentPane.add(new JScrollPane(tree), BorderLayout.CENTER);
 
         // make button panel
-        contentPane.add(makeButtonPanel(), "South");
+        contentPane.add(makeButtonPanel(), BorderLayout.SOUTH);
+
+        tree.setComponentPopupMenu(makePopupMenu());
+    }
+
+    private JPopupMenu makePopupMenu() {
+        JPopupMenu popupMenu = new JPopupMenu("Title");
+        popupMenu.add(addChildButton.getAction());
+        popupMenu.add(addSiblingButton.getAction());
+        popupMenu.add(deleteButton.getAction());
+        return popupMenu;
     }
 
     private JPanel makeButtonPanel() {
         JPanel panel = new JPanel();
-        addSiblingButton = new JButton("Add Sibling");
-        addSiblingButton.addActionListener(this);
+        addSiblingButton = new JButton(new AddSiblingAction());
         panel.add(addSiblingButton);
-        addChildButton = new JButton("Add Child");
-        addChildButton.addActionListener(this);
+        addChildButton = new JButton(new AddChildAction());
         panel.add(addChildButton);
-        deleteButton = new JButton("Delete");
-        deleteButton.addActionListener(this);
+        deleteButton = new JButton(new DeleteNodeAction());
         panel.add(deleteButton);
         return panel;
     }
@@ -100,41 +93,66 @@ class TreeEditFrame extends JFrame implements ActionListener {
         return root;
     }
 
-    public void actionPerformed(ActionEvent event) {
-        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree
-                .getLastSelectedPathComponent();
-
-        if (selectedNode == null)
-            return;
-
-        if (event.getSource().equals(deleteButton)) {
-            if (selectedNode.getParent() != null)
-                model.removeNodeFromParent(selectedNode);
-            return;
+    private class AddChildAction extends AbstractAction {
+        private AddChildAction() {
+            putValue(Action.NAME, "Add Child");
         }
 
-        // add new node as sibling or child
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+            if (selectedNode == null) return;
 
-        DefaultMutableTreeNode newNode = new DefaultMutableTreeNode("New");
+            MutableTreeNode newNode = new DefaultMutableTreeNode("New");
+            model.insertNodeInto(newNode, selectedNode, selectedNode.getChildCount());
 
-        if (event.getSource().equals(addSiblingButton)) {
-            DefaultMutableTreeNode parent = (DefaultMutableTreeNode) selectedNode
-                    .getParent();
+            TreeNode[] nodes = model.getPathToRoot(newNode);
+            TreePath path = new TreePath(nodes);
+            tree.scrollPathToVisible(path);
+        }
+    }
+
+    private class AddSiblingAction extends AbstractAction {
+        private AddSiblingAction() {
+            putValue(Action.NAME, "Add Sibling");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+            if (selectedNode == null) return;
+            MutableTreeNode newNode = new DefaultMutableTreeNode("New");
+
+            DefaultMutableTreeNode parent = (DefaultMutableTreeNode) selectedNode.getParent();
 
             if (parent != null) {
                 int selectedIndex = parent.getIndex(selectedNode);
                 model.insertNodeInto(newNode, parent, selectedIndex + 1);
             }
-        } else if (event.getSource().equals(addChildButton)) {
-            model.insertNodeInto(newNode, selectedNode, selectedNode
-                    .getChildCount());
+            TreeNode[] nodes = model.getPathToRoot(newNode);
+            TreePath path = new TreePath(nodes);
+            tree.scrollPathToVisible(path);
+
+        }
+    }
+
+    private class DeleteNodeAction extends AbstractAction {
+        private DeleteNodeAction() {
+            putValue(Action.NAME, "Delete");
         }
 
-        // now display new node
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+            if (selectedNode == null)
+                return;
 
-        TreeNode[] nodes = model.getPathToRoot(newNode);
-        TreePath path = new TreePath(nodes);
-        tree.scrollPathToVisible(path);
+
+            if (selectedNode.getParent() != null)
+                model.removeNodeFromParent(selectedNode);
+
+
+        }
     }
 }
 
