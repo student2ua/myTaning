aa2feec05a1eaabadb2a8ea58fc37bab7fd94f25
student2ua <student2ua@gmail.com>
2017-02-18 21:42:17 +0200

    Асинхронные сервлеты Servlet 3.0 (JSR 315)

--- webExample/examples/src/main/java/com/tor/web/asyncServlet/AsyncServlet_StartAsync.java
+++ webExample/examples/src/main/java/com/tor/web/asyncServlet/AsyncServlet_StartAsync.java
@@ -4,6 +4,8 @@ import javax.servlet.AsyncContext;
 import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,12 +16,16 @@ import java.io.IOException;
  * User: Tor
  * Date: 18.02.17
  * Time: 18:31
- * Servlet 3.0 (JSR 315), Tomcat 6+
+ * Servlet 3.0 (JSR 315), Servlet API 3.0 – compliant application server such as Tomcat 7+, JBoss 6 or Jetty 8+
  * http://pro-java.ru/java-dlya-opytnyx/asinxronnye-servlety-java/
  * Спецификация Servlet 3.0 предоставила метод startAsync(), сделавший доступными асинхронные операции.
  * Метод onComplete класса Asynclistener выполняется только после завершения выполнения.
  */
 @WebServlet(urlPatterns = {"/async"}, asyncSupported = true)
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"admin"})) //стало возможно задавать ограничения доступа
+/*@ServletSecurity(httpMethodConstraints={
+        @HttpMethodConstraint("GET"),
+        @HttpMethodConstraint(value="POST", rolesAllowed={"user_group_1"})})*/
 public class AsyncServlet_StartAsync extends HttpServlet {
 
     @Override
@@ -42,6 +48,10 @@ public class AsyncServlet_StartAsync extends HttpServlet {
             @Override
             public void onTimeout(AsyncEvent event) throws IOException {
                 // выполняется, только если истекает время ожидания
+                event.getAsyncContext()
+                        .getResponse()
+                        .getWriter()
+                        .write("TimeOut Error in Processing");
             }
 
             @Override
