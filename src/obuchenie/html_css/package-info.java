34715d227d17e7f7da47a9389928bd983e5dff09
tor <student2ua@gmail.com>
2023-12-12 21:11:32 +0200

    html & css

---
+++ src/obuchenie/html_css/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * User: tor
+ * Date: 004, 04.10.2023
+ * Time: 18:57
+ * <ul>
+ * <li>{@link  .\applicationCacheAPI\}     https://habr.com/ru/articles/151815/
+ * <li>{@link  .\border\}     cool border using `background-image: linear-gradient`
+ * <li>{@link  .\castomingView\}
+ * <li>{@link  .\glassmorphism\}
+ * <li>{@link .\registerForm.demo1}
+ * <li>{@link .\sokets\}
+ * <li>{@link .\viewTransitionAPI}  View Transitions API Example
+ * <li>{@link .\hideDiv.html} -     Toggle example
+ * <li>{@link .\html5.html}         header, hgroup,footer, figure,  mark
+ * <li>{@link .\html5svg.html}      svg
+ * <li>{@link .\placeholder.html}   svg in input + not work custom html tag
+ * <li>{@link .\VisitedLinksStrike.html}
+ * <li>{@link .\FormEventBlock.html.html} [Не нужно блокировать кнопки](https://habr.com/ru/companies/productivity_inside/articles/775648/)
+ * <li>{@link .\FormEventBlock.html.html} [1](https://habr.com/ru/companies/ruvds/articles/779814/) [YT Stop using JavaScript for that](https://www.youtube.com/watch?v=ZTMUJu26b7Q)
+ * </ul>
+ */
+
+package obuchenie.html_css;
